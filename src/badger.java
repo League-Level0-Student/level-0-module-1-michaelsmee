8d9fda345d1dbcc310278c7aa8173afb4b12eaaa
league <league@worthymini-5.attlocal.net>
2018-06-30 12:47:45 -0700

    i did badger and i also did age

---
+++ src/badger.java
@@ -0,0 +1,50 @@
+
+public class badger {
+	public static void main(String [] args) {	
+	
+		
+		for (int z = 1; z<2;z++) {
+		for (int y=1; y<3; y++) {
+			String b="badger";
+			for (int i=1;i<17;i++){
+
+				System.out.println(b);	
+				
+			}
+				for (int x=1; x<3; x++) {		
+					String m="mushroom";
+					System.out.println(m);{	
+					}
+
+				}
+			
+		
+	}
+		for (int y=1; y<3; y++) {
+			String b="badger";
+			for (int i=1;i<17;i++){
+
+				System.out.println(b);	
+
+	}
+for (int s=1;s<3;s++) {
+String p="Snake";
+for(int s1=1; s1<3;s1++) {
+	System.out.println(p);
+
+}
+System.out.println("oooooooooooooo"+p);
+}
+}
+		}
+	}
+	}
+
+
+
+
+
+
+
+
+
